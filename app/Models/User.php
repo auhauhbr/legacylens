1336f32bc8e6e9df240afc955bf7cc08b83e41b1
Jefferson Tadeu dos Santos <tadeu.santos7148@gmail.com>
2026-06-29 18:32:25 -0300

    laravel: inicia estrutura base com autenticação

---
+++ app/Models/User.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+// Use MustVerifyEmail quando a verificação de e-mail for habilitada.
+use Database\Factories\UserFactory;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Attributes\Hidden;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+#[Fillable(['name', 'email', 'password'])]
+#[Hidden(['password', 'remember_token'])]
+class User extends Authenticatable implements FilamentUser
+{
+    /** @use HasFactory<UserFactory> */
+    use HasFactory, Notifiable;
+
+    public function projetos(): HasMany
+    {
+        return $this->hasMany(Projeto::class, 'usuario_id');
+    }
+
+    public function canAccessPanel(Panel $painel): bool
+    {
+        return true;
+    }
+
+    /**
+     * Retorna os atributos que devem receber conversão de tipo.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+}
